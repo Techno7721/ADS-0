e9cf99a5c3f9a47fa6221e80e41abe03b349dc2e
Осинин Ярослав <99194733+Techno7721@users.noreply.github.com>
2022-02-13 20:13:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,24 +2,18 @@
 
 int gcd(int a, int b) {
     int men = 0;
-	int nod = 0;
-	if (a > b)
-	{
-		men = b;
-	}
-	else
-	{
-		men = a;
-	}
-	for (int i = 1;i < men + 1;++i)
-	{
-		if (a % i == 0)
-		{
-			if (b % i == 0)
-			{
-				nod = i;
-			}
-		}
-	}
-	return nod;
+    int nod = 0;
+    if (a > b) {
+        men = b;
+    }else {
+        men = a;
+    }
+    for (int i = 1; i < men + 1;++i) {
+        if (a % i == 0) {
+            if (b % i == 0) {
+                nod = i;
+            }
+        }
+    }
+    return nod;
 }
